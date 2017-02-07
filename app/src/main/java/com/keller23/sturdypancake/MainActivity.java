12e58ac41f0b35f6115703135db8de867d34435c
shaynemk <uskickbutt@gmail.com>
2017-02-07 01:37:27 -0500

    updated the build numbers, filename and added firebase.

--- app/src/main/java/com/keller23/sturdypancake/MainActivity.java
+++ app/src/main/java/com/keller23/sturdypancake/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.i(getString(R.string.app_name),"Welcome to the Sturdy Pancake!");
     }
 
     @Override
@@ -44,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings in Progress", Toast.LENGTH_LONG).show();
             return true;
         }
 
